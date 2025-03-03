6d44559b56c9d458ac0aefdb9bf6bdc51f9c87b2
xonyis <julianmayerpro@outlook.fr>
2025-03-04 00:50:51 +0100

    test

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -2,17 +2,27 @@
 
 import type React from "react"
 
+import { Input } from "@/components/ui/input"
+import {Card, CardContent} from "@/components/ui/card";
 
 export default function HomePage() {
 
     return (
         <div className="min-h-screen bg-[#fdf2e3]">
-            <div className="w-full">
-                <div className="overflow-hidden">
-                    fff
-                </div>
+            <div className="w-full flex justify-center  content-center p-6">
+                <FormCard/>
             </div>
         </div>
+    )
+}
+
+function FormCard() {
+    return (
+        <Card className="w-200 group border-[#921600]/10 transition-all ease-in-out duration-350  hover:border-[#921600]/30 hover:shadow-lg">
+            <CardContent className="p-6"  >
+                yo
+            </CardContent>
+        </Card>
     )
 }
 
