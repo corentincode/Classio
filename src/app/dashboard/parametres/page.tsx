ca5ac0529f2a0d2f41bcc9269de49b881d44fb35
corentincode <corentinadlpro@gmail.com>
2025-03-31 16:28:11 +0200

    Pages Create on dashboard fix bug 6

--- src/app/dashboard/parametres/page.tsx
+++ src/app/dashboard/parametres/page.tsx
@@ -1,4 +1,4 @@
-import SettingsContent from "@/components/dashboard/settings-content"
+import SettingsContent from "@/components/dashboard/admin/settings/settings-content"
 
 export default function SettingsPage() {
   return <SettingsContent />
